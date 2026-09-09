1824281f5f323d1ccaf655f531fbd5324b4578ab
natanim-samuel <ntnmsamuel@gmail.com>
2026-09-09 18:21:24 +0300

    screen

---
+++ lib/screens/screen1.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class screen1 extends StatelessWidget {
+  const screen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+         color: Colors.purpleAccent,
+
+        ),
+      )
+    );
+  }
+}
+
+
